c7ed3f85366adc7d486063a4cb69002b8de18475
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-02 12:11:12 +0900

    Change Check

--- Check.py
+++ Check.py
@@ -1,21 +1,25 @@
-# Usage : Python Check.py [Directory Name(e.g. C, C++, C#, Python ...)] [File Extension(e.g. c, cpp, cs, py ...)] <1000> <1001> <1002> ... <???>
+# Usage : Python Check.py <1000> <1001> <1002> ... <???>
 # Description : This file checks for missing files.
 
 from sys import argv
 from os import path
 
-if __name__ != "__main__":
-    exit()
-
 argv.pop(0)
 
-directoryName = argv.pop(0)
-fileExtension = argv.pop(0)
-noneList = []
+if __name__ != "__main__" or len(argv) == 0:
+    exit()
 
-for arg in argv:
-    if not path.exists(path.join(directoryName, f"{arg}.{fileExtension}")):
-        noneList.append(arg)
+directories = ["C", "C++", "Python", "아희"]
+fileExtensions = ["c", "cpp", "py", "aheui"]
 
-for none in noneList:
-    print(none)
+for directory, extension in zip(directories, fileExtensions):
+    print(f"[{directory}]")
+    print("----------")
+
+    for arg in argv:
+        fileName = f"{arg}.{extension}"
+        
+        if not path.exists(path.join(directory, fileName)):
+            print(fileName)
+
+    print()
