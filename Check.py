517a48cbdc4e5a95ed4369553ff3f9e9ab5f3fc4
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-04-07 07:25:40 +0900

    Add Golfscript in Check.py
    The Goldscript directory was added to Check.py because it's funny!

--- Check.py
+++ Check.py
@@ -9,8 +9,8 @@ argv.pop(0)
 if __name__ != "__main__" or len(argv) == 0:
     exit()
 
-directories = ["C", "C++", "Python", "아희", "FreeBASIC", "Ada", "Assembly"]
-fileExtensions = ["c", "cpp", "py", "aheui", "bas", "adb", "asm"]
+directories = ["C", "C++", "Python", "아희", "FreeBASIC", "Ada", "Assembly", "Golfscript"]
+fileExtensions = ["c", "cpp", "py", "aheui", "bas", "adb", "asm", "gs"]
 
 for directory, extension in zip(directories, fileExtensions):
     print(f"[{directory}]")
