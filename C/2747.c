6c4c4dc41d284b9b5cac4f7df822ae69b62f0c53
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-04-12 17:36:08 +0900

    Solve 2747

---
+++ C/2747.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int array[17] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597 };
+
+int Fibonacci(int n);
+
+int main(void)
+{
+	int n;
+	
+	scanf("%d", &n);
+	printf("%d", Fibonacci(n));
+	
+	return 0;
+}
+
+int Fibonacci(int n)
+{
+	int i;
+	
+	if (n < 17)
+	{
+		return array[n];
+	}
+	
+	return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
