77489bff443996f81abd03846579566172084a92
JkhTV <candzzang5283@gmail.com>
2025-12-21 15:55:53 +0900

    Solve(Java) 2557
    Add Java directory

--- Check.py
+++ Check.py
@@ -9,18 +9,18 @@ argv.pop(0)
 if __name__ != "__main__" or len(argv) == 0:
     exit()
 
-directories = ["C", "C++", "Python", "아희", "FreeBASIC", "Ada", "Assembly", "Golfscript", "VisualBasic"]
-fileExtensions = ["c", "cpp", "py", "aheui", "bas", "adb", "asm", "gs", "vb"]
+directories = ["C", "C++", "Python", "Java", "아희", "FreeBASIC", "Ada", "Assembly", "Golfscript", "VisualBasic"]
+fileExtensions = ["c", "cpp", "py", "java", "aheui", "bas", "adb", "asm", "gs", "vb"]
 
 for directory, extension in zip(directories, fileExtensions):
     print(f"[{directory}]")
     print("----------")
 
     count = 0
-    
+
     for arg in argv:
         fileName = f"{arg}.{extension}"
-        
+
         if not path.exists(path.join(directory, fileName)):
             count += 1
 
