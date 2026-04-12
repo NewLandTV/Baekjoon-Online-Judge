ce1fe0de3ee16280bd06634c79c8a7677460634f
JkhTV <candzzang5283@gmail.com>
2026-04-12 13:34:37 +0900

    Resolve(Python) 20444

--- Python/20444.py
+++ Python/20444.py
@@ -1,13 +1,6 @@
 n, k = map(int, input().split())
-
 d = n ** 2 + (n - k + 1) * 4
 if d < 0 or d != int(d ** 0.5) ** 2:
     print("NO")
-    exit()
-
-a, b = (n + d ** 0.5) * 0.5, (n - d ** 5) * 0.5
-if a != int(a) or b != int(b):
-    print("NO")
-    exit()
-
-print("YES")
+else:
+    print("YES")
